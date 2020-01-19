766af9e1501fffea15e76e7cca8cd6c738864669
cytmike <36017298+cytmike@users.noreply.github.com>
2020-01-18 20:09:54 -0800

    Create 344.py

---
+++ 344.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(0,int(len(s)/2)):
+            z=s[i]
+            s[i]=s[len(s)-i-1]
+            s[len(s)-i-1]=z
+        
