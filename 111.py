140275acaf4b916f4499d4a68e85dbfcdc90271f
cytmike <36017298+cytmike@users.noreply.github.com>
2020-02-19 16:28:50 -0800

    Create 111.py

---
+++ 111.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root==None:
+            return 0
+        if root.left==None:
+            return 1+self.minDepth(root.right)
+        if root.right==None:
+            return 1+self.minDepth(root.left)
+        return 1+min(self.minDepth(root.left),self.minDepth(root.right))
